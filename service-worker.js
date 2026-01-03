f35acc738ab5ad2deb322642a8df275e53cc4343
IBEMG <comunicacao.ibemanuelmg@gmail.com>
2026-01-03 10:45:24 -0300

    Add caching for initial resources in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,12 @@
 
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("ibemg-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/logo.png"
+      ]);
+    })
+  );
+});
